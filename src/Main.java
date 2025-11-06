5829aaeb8f03d431768301dcf741435e0a00b40a
imbipin <bipinka01@gmail.com>
2025-11-06 12:27:40 +0545

    Day 2: Constructors, Encapsulation, and Method Overloading

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,71 @@
 import Day1_JavaBasics.*;
+import Day2_OOP_Basics.MehodOverloading.Calculator;
 import Day2_OOP_Basics.constructors.DefaultConstructors;
 import Day2_OOP_Basics.constructors.ParameterizedConstructors;
+import Day2_OOP_Basics.encapsulation.Account;
 
 public class Main {
     public static void main(String[] args) {
 
 //        Day 1
 
-
 //         System.out.println("Hello and welcome!");
 
-        VariablesAndDataTypes obj1 = new VariablesAndDataTypes();
+//        VariablesAndDataTypes obj1 = new VariablesAndDataTypes();
 //        obj1.printAge();
 
-        Operators obj2 = new Operators();
+//        Operators obj2 = new Operators();
 //        obj2.operations();
 
-        Conditionals obj3 = new Conditionals();
+//        Conditionals obj3 = new Conditionals();
 //        obj3.printGrade(50);
 
-        Loops obj4 = new Loops();
+//        Loops obj4 = new Loops();
 //        obj4.forLoop();
 //        obj4.whileLoop();
 
-        Functions obj5 = new Functions();
+//        Functions obj5 = new Functions();
 //        int result = obj5.demo(14,5);
 //        System.out.println(result);
 
-        ClassesAndObjects obj6 = new ClassesAndObjects();
+//        ClassesAndObjects obj6 = new ClassesAndObjects();
 //        obj6.demoMain();
 
 
 //        Day 2
 
-        DefaultConstructors c1 = new DefaultConstructors();
+//        DefaultConstructors c1 = new DefaultConstructors();
 //        c1.display();
 
-        ParameterizedConstructors c2 = new ParameterizedConstructors("Bipin", 25);
-        c2.displayStudentDetails();
+//        ParameterizedConstructors c2 = new ParameterizedConstructors("Bipin", 25);
+//        c2.displayStudentDetails();
 
+//        Account e1 = new Account("Bipin", 0);
+//        Account e2 = new Account("Sanbi", 2000);
+//        Account e3 = new Account("Bisan)", 2000);
+
+//        System.out.println("Before: ");
+//        e1.showDetails();
+//        e2.showDetails();
+//        e3.showDetails();
+
+//        e1.deposit(5000);
+//        e2.withdraw(500);
+//        e3.deposit(900);
+
+//        System.out.println("After: ");
+//        e1.showDetails();
+//        e2.showDetails();
+//        e3.showDetails();
+
+        Calculator calc = new Calculator();
+        System.out.println("\nSum of two integers: " + calc.add(5, 10));
+        System.out.println("\nMethod overloading - Changing the Number of Parameters");
+        System.out.println("Sum of three integers: " + calc.add(10, 20, 30));
+        System.out.println("\nMethod overloading - Changing Data Types of Parameters");
+        System.out.println("Sum of two decimal numbers: " + calc.add(5.5, 8.2));
+        System.out.println("\n\nMethod overloading - Changing the Order of Parameters");
+        calc.student("Bipin", 1);
+        calc.student(2, "Sanbi");
     }
 }
