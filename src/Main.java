48d22c93e7a7ec1ae3dfe16b806cb21f5c47452e
imbipin <bipinka01@gmail.com>
2026-02-08 22:48:09 +0545

    Day-4 Hash Map

--- src/Main.java
+++ src/Main.java
@@ -1,151 +1,151 @@
-import Day2_OOP_Basics.methodOverloading.Calculator;
-import Day3_OOP_Concepts.abstraction.abstraceClass.Cat;
-import Day3_OOP_Concepts.abstraction.interfaces.Car;
-import Day3_OOP_Concepts.abstraction.interfaces.Vehicle;
-import Day3_OOP_Concepts.employeeManagementSystem.Developer;
-import Day3_OOP_Concepts.employeeManagementSystem.Employee;
-import Day3_OOP_Concepts.employeeManagementSystem.Manager;
-import Day3_OOP_Concepts.inheritance.basic.Student;
-import Day3_OOP_Concepts.inheritance.usingConstructor.Teacher;
-import Day3_OOP_Concepts.methodOverriding.Animal;
-import Day3_OOP_Concepts.methodOverriding.Dog;
-import Day4_CollectionsFramework.listInterface.ArrayLists;
-import Day4_CollectionsFramework.listInterface.LinkedLists;
-import Day4_CollectionsFramework.mapInterface.HashMaps;
-import Day4_CollectionsFramework.setInterface.HashSets;
-
-public class Main {
-    public static void main(String[] args) {
-
-//        Day 1
-
-//         System.out.println("Hello and welcome!");
-
-//        VariablesAndDataTypes obj1 = new VariablesAndDataTypes();
-//        obj1.printAge();
-
-//        Operators obj2 = new Operators();
-//        obj2.operations();
-
-//        Conditionals obj3 = new Conditionals();
-//        obj3.printGrade(50);
-
-//        Loops obj4 = new Loops();
-//        obj4.forLoop();
-//        obj4.whileLoop();
-
-//        Functions obj5 = new Functions();
-//        int result = obj5.demo(14,5);
-//        System.out.println(result);
-
-//        ClassesAndObjects obj6 = new ClassesAndObjects();
-//        obj6.demoMain();
-
-
-//        Day 2
-
-//        DefaultConstructors c1 = new DefaultConstructors();
-//        c1.display();
-
-//        ParameterizedConstructors c2 = new ParameterizedConstructors("Bipin", 25);
-//        c2.displayStudentDetails();
-
-//        Account e1 = new Account("Bipin", 0);
-//        Account e2 = new Account("Sanbi", 2000);
-//        Account e3 = new Account("Bisan)", 2000);
-
-//        System.out.println("Before: ");
-//        e1.showDetails();
-//        e2.showDetails();
-//        e3.showDetails();
-
-//        e1.deposit(5000);
-//        e2.withdraw(500);
-//        e3.deposit(900);
-
-//        System.out.println("After: ");
-//        e1.showDetails();
-//        e2.showDetails();
-//        e3.showDetails();
-
-//        Day 2 - Method Overloading
-
-//        Calculator calc = new Calculator();
-//        System.out.println("\nSum of two integers: " + calc.add(5, 10));
-//        System.out.println("\nMethod overloading - Changing the Number of Parameters");
-//        System.out.println("Sum of three integers: " + calc.add(10, 20, 30));
-//        System.out.println("\nMethod overloading - Changing Data Types of Parameters");
-//        System.out.println("Sum of two decimal numbers: " + calc.add(5.5, 8.2));
-//        System.out.println("\n\nMethod overloading - Changing the Order of Parameters");
-//        calc.student("Bipin", 1);
-//        calc.student(2, "Sanbi");
-
-
-//        Day-3 Basic Inheritance
-
-//        Student s1 = new Student();
-//        s1.name = "Bipin";
-//        s1.age = 25;
-//        s1.college = "LEC";
-//        s1.showInfo();
-//        s1.showCollege();
-
-
-//        Day-3 Inheritance using Constructor
-
-//        Teacher t1 = new Teacher("Ram", 45, "LEC");
-//        t1.showDetails();
-
-
-//        Day-3 Method Overriding
-
-//        Animal d= new Dog();
-//        d.makeSound();
-
-
-//        Day-3 Abstraction - abstract class
-
-//        Animal a = new Cat();
-//        a.makeSound();
-//        a.sleep();
-
-
-//        Day-3 interface
-
-//        Vehicle v = new Car();
-//        v.start();
-//        v.stop();
-
-
-//        Day-3 Mini Project - Employee Management System
-
-//        Employee e1 = new Developer("Bipin", 200000);
-//        Employee e2 = new Manager("Sita", 210000);
-//        e1.showDetails();
-//        e1.calculateBonus();
-//        e2.showDetails();
-//        e2.calculateBonus();
-
-
-//        Day-4 Array Lists
-//        ArrayLists arr = new ArrayLists();
-//        arr.result();
-
-
-//        Day-4 Linked List
-//        LinkedLists l = new LinkedLists();
-//        l.result();
-
-
-//        Day-4 Hash Set
-//        HashSets hSet = new HashSets();
-//        hSet.result();
-
-//        Day-4 Hash Map
-//        HashMaps hMap = new HashMaps();
-//        hMap.result();
-
-
-
-    }
-}
+//import Day2_OOP_Basics.methodOverloading.Calculator;
+//import Day3_OOP_Concepts.abstraction.abstraceClass.Cat;
+//import Day3_OOP_Concepts.abstraction.interfaces.Car;
+//import Day3_OOP_Concepts.abstraction.interfaces.Vehicle;
+//import Day3_OOP_Concepts.employeeManagementSystem.Developer;
+//import Day3_OOP_Concepts.employeeManagementSystem.Employee;
+//import Day3_OOP_Concepts.employeeManagementSystem.Manager;
+//import Day3_OOP_Concepts.inheritance.basic.Student;
+//import Day3_OOP_Concepts.inheritance.usingConstructor.Teacher;
+//import Day3_OOP_Concepts.methodOverriding.Animal;
+//import Day3_OOP_Concepts.methodOverriding.Dog;
+//import Day4_CollectionsFramework.listInterface.ArrayLists;
+//import Day4_CollectionsFramework.listInterface.LinkedLists;
+//import Day4_CollectionsFramework.mapInterface.HashMaps;
+//import Day4_CollectionsFramework.setInterface.HashSets;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//
+////        Day 1
+//
+////         System.out.println("Hello and welcome!");
+//
+////        VariablesAndDataTypes obj1 = new VariablesAndDataTypes();
+////        obj1.printAge();
+//
+////        Operators obj2 = new Operators();
+////        obj2.operations();
+//
+////        Conditionals obj3 = new Conditionals();
+////        obj3.printGrade(50);
+//
+////        Loops obj4 = new Loops();
+////        obj4.forLoop();
+////        obj4.whileLoop();
+//
+////        Functions obj5 = new Functions();
+////        int result = obj5.demo(14,5);
+////        System.out.println(result);
+//
+////        ClassesAndObjects obj6 = new ClassesAndObjects();
+////        obj6.demoMain();
+//
+//
+////        Day 2
+//
+////        DefaultConstructors c1 = new DefaultConstructors();
+////        c1.display();
+//
+////        ParameterizedConstructors c2 = new ParameterizedConstructors("Bipin", 25);
+////        c2.displayStudentDetails();
+//
+////        Account e1 = new Account("Bipin", 0);
+////        Account e2 = new Account("Sanbi", 2000);
+////        Account e3 = new Account("Bisan)", 2000);
+//
+////        System.out.println("Before: ");
+////        e1.showDetails();
+////        e2.showDetails();
+////        e3.showDetails();
+//
+////        e1.deposit(5000);
+////        e2.withdraw(500);
+////        e3.deposit(900);
+//
+////        System.out.println("After: ");
+////        e1.showDetails();
+////        e2.showDetails();
+////        e3.showDetails();
+//
+////        Day 2 - Method Overloading
+//
+////        Calculator calc = new Calculator();
+////        System.out.println("\nSum of two integers: " + calc.add(5, 10));
+////        System.out.println("\nMethod overloading - Changing the Number of Parameters");
+////        System.out.println("Sum of three integers: " + calc.add(10, 20, 30));
+////        System.out.println("\nMethod overloading - Changing Data Types of Parameters");
+////        System.out.println("Sum of two decimal numbers: " + calc.add(5.5, 8.2));
+////        System.out.println("\n\nMethod overloading - Changing the Order of Parameters");
+////        calc.student("Bipin", 1);
+////        calc.student(2, "Sanbi");
+//
+//
+////        Day-3 Basic Inheritance
+//
+////        Student s1 = new Student();
+////        s1.name = "Bipin";
+////        s1.age = 25;
+////        s1.college = "LEC";
+////        s1.showInfo();
+////        s1.showCollege();
+//
+//
+////        Day-3 Inheritance using Constructor
+//
+////        Teacher t1 = new Teacher("Ram", 45, "LEC");
+////        t1.showDetails();
+//
+//
+////        Day-3 Method Overriding
+//
+////        Animal d= new Dog();
+////        d.makeSound();
+//
+//
+////        Day-3 Abstraction - abstract class
+//
+////        Animal a = new Cat();
+////        a.makeSound();
+////        a.sleep();
+//
+//
+////        Day-3 interface
+//
+////        Vehicle v = new Car();
+////        v.start();
+////        v.stop();
+//
+//
+////        Day-3 Mini Project - Employee Management System
+//
+////        Employee e1 = new Developer("Bipin", 200000);
+////        Employee e2 = new Manager("Sita", 210000);
+////        e1.showDetails();
+////        e1.calculateBonus();
+////        e2.showDetails();
+////        e2.calculateBonus();
+//
+//
+////        Day-4 Array Lists
+////        ArrayLists arr = new ArrayLists();
+////        arr.result();
+//
+//
+////        Day-4 Linked List
+////        LinkedLists l = new LinkedLists();
+////        l.result();
+//
+//
+////        Day-4 Hash Set
+////        HashSets hSet = new HashSets();
+////        hSet.result();
+//
+////        Day-4 Hash Map
+////        HashMaps hMap = new HashMaps();
+////        hMap.result();
+//
+//
+//
+//    }
+//}
